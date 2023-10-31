ac9b4cbb3e00a6a3151adcb6041e366fee8f324a
kaurtejwant998 <27883229-kaurtejwant998@users.noreply.replit.com>
2023-10-31 10:46:46 +0000

    Initial commit

---
+++ day1/main.py
@@ -0,0 +1,29 @@
+# Write your code below this line 👇
+print("Tejwant Kaur")
+# print("print('What to print('Hello') do')")
+
+# print ("I am here")
+# print("Oh my Gosh!!");
+
+# print ("Hello " + input("Whats Yur name? "))
+# print (len(input("Whats Yur name? ")) )
+
+fname = input("Enter First Name: ")
+lname = input("Enter Last Name: ")
+print(fname + " " + lname)
+
+city = input("Enter yur City name: ")
+pet = input("Enter yur Pet's name: ")
+print("Yur Band Name is " + city + " " + pet)
+
+a = 5
+b = 4
+print("\na = " + str(a))
+print("b = " + str(b))
+
+c = a
+a = b
+b = c
+print("\nAfter Swapping: ")
+print("a = " + str(a))
+print("b = " + str(b))
---
+++ day2/main.py
@@ -0,0 +1,25 @@
+# print("hello"[3])
+
+# print(123+456)
+
+# print(type(input("Whats yur name: ")))
+
+# numChar = str(len(input("Whats yur name: ")))
+# print("Your name has " + numChar + " characters")
+
+# print(173 + float("23"))
+# print(str(27) + "700")
+
+# two_digit = input("Enter two digits: ")
+# print(type(two_digit))
+
+# f_dig = two_digit[0]
+# s_dig = two_digit[1]
+
+# result = int(f_dig) + int(s_dig)
+# print("The result of sum of digits of " + two_digit + " is " + str(result))
+
+print(4 ** 2)
+
+
+
