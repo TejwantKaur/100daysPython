6d8e74968ed0974d175e6bbb1c89437928725b88
TejwantKaur <114721213+TejwantKaur@users.noreply.github.com>
2023-11-04 16:20:22 +0000

    day4

---
+++ day4/main.py
@@ -0,0 +1,10 @@
+from module1file import *
+import random
+
+print(pis)
+
+randomInt = random.randint(1,10)
+randomFloat = random.random() # 0.00111 -- 0.9999999
+
+print(randomInt)
+print(randomFloat)
