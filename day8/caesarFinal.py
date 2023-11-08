69e9c332f8e1b757617b539e3d7b258b41634cfc
TejwantKaur <114721213+TejwantKaur@users.noreply.github.com>
2023-11-08 11:54:38 +0000

    final

--- day8/caesarFinal.py
+++ day8/caesarFinal.py
@@ -1,6 +1,6 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g','h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-def caesar(text,shift,direction):
+def caesar(text,shift,dir):
   end_text = ""
 
   if dir == 'de':
@@ -11,6 +11,28 @@ def caesar(text,shift,direction):
       position = alphabet.index(i)
       new_position = position + shift
       end_text += alphabet[new_position]
+    else:
+      end_text += i
+
+  print(f"Here's the {dir} text is {end_text}")
+
+from art import logo
+print(logo)
+
+end = False
+while not end:
+
+  dir = input("Type en to encypt and de to decrypt: ")
+  text = input("Enter text: ")
+  shift = int(input("Type shift num: "))
+
+  shift = shift % 26
+  caesar(text,shift,dir)
+
+  restart = input("Type yes 'y' if you wanna continue else 'n' ")
+  if restart == 'n':
+    end = True
+    print('GoodBye')
     
     
     
