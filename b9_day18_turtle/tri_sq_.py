86bf6080a1d107af1facac352af878d9b11cfc93
TejwantKaur <kaurtejwant998@gmail.com>
2023-11-15 12:02:58 +0530

    tri square pentagon hexagon.... added function... added colors

--- b9_day18_turtle/tri_sq_.py
+++ b9_day18_turtle/tri_sq_.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+import random
 
 sc = Screen()
 tim = Turtle()
@@ -12,6 +13,12 @@ tim = Turtle()
 #         tim.right(angle)
 #     i += 1
 
+colors = [
+    "red", "green", "blue", "yellow", "purple",
+    "orange", "pink", "brown", "cyan", "lime",
+    "teal", "magenta", "black", "gray",
+    "lightblue", "darkgreen", "gold", "violet", "turquoise",
+]
 # line --> kini line draw kerni? tri-->3 , sq-->4 , pen-->5
 def draw(lines):
     angle = 360 / lines
@@ -19,8 +26,8 @@ def draw(lines):
         tim.forward(50)
         tim.right(angle)
 
-
-for num in range(3, 15):  # 11 not included 3-->tri, 4-->sq
+for num in range(3, 20):  # 11 not included 3-->tri, 4-->sq
+    tim.color(random.choice(colors))
     draw(num)
 
 sc.exitonclick()
