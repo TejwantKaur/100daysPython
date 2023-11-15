c2af499e98d5958dfdf0e5d75217148c39dabb39
TejwantKaur <kaurtejwant998@gmail.com>
2023-11-15 14:18:38 +0530

    colors as tuple (..., ..., ...)

--- b9_day18_hirs_painting/main.py
+++ b9_day18_hirs_painting/main.py
@@ -4,8 +4,11 @@ import colorgram
 colors = colorgram.extract('image.jpg', 6)
 
 rgb_colors = []
-# rgb
 for color in colors:
-    rgb_colors.append(color.rgb)
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    new_color = (r, g, b)
+    rgb_colors.append(new_color)
 
 print(rgb_colors)
