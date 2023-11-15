792512344356a4773b4d46aac8fb2a58d85bd86b
TejwantKaur <kaurtejwant998@gmail.com>
2023-11-15 14:13:01 +0530

    direction turtle random : direction, random color... rgb

--- b9_day18_turtle/direction.py
+++ b9_day18_turtle/direction.py
@@ -4,46 +4,23 @@ import random
 sc = Screen()
 tim = Turtle()
 colormode(255)
+tim.speed("fastest")
 
 def random_color():
     r = random.randint(0,255)
     g = random.randint(0, 255)
     b = random.randint(0, 255)
-#     create tuple
-    rand_color = (r, g, b)
-    return rand_color
+    color = (r, g, b)
+    return color
 
-# colors = [
-#     "red", "green", "blue", "yellow", "purple",
-#     "orange", "pink", "brown", "cyan", "lime",
-#     "teal", "magenta", "black", "gray",
-#     "lightblue", "darkgreen", "gold", "violet", "turquoise",
-# ]
-
-
-direction = [0, 90, 180, 270]
-tim.pensize(7)
 tim.speed("fastest")
+tim.circle(100)
 
-for _ in range(200):
-    tim.color(random_color())
-    tim.forward(20)
-    tim.setheading(random.choice(direction))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def drawGraph(angle):
+    for _ in range(100):
+        tim.color(random_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + 5)
 
 
 
