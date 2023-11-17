cf7c1efabd1b3f9c75710e86820b71eb9a7f351a
TejwantKaur <kaurtejwant998@gmail.com>
2023-11-17 17:29:18 +0530

    snake game :)

---
+++ c2_day20_snake_game/main.py
@@ -0,0 +1,21 @@
+from turtle import Screen, Turtle
+
+sc = Screen()
+sc.setup(600,600)
+sc.bgcolor("black")
+sc.title("My Snake Game :)")
+
+t1 = Turtle("square")
+t1.color("white")
+t2 = Turtle("square")
+t2.setposition((-20, 0))
+t3 = Turtle("square")
+
+
+
+
+
+
+
+
+sc.exitonclick()
