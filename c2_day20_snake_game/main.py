c5b357fb218c66e93f641fd4161c2d60f13bf2ae
TejwantKaur <kaurtejwant998@gmail.com>
2023-11-17 20:03:20 +0530

    snake game :)

--- c2_day20_snake_game/main.py
+++ c2_day20_snake_game/main.py
@@ -1,21 +1,29 @@
 from turtle import Screen, Turtle
+import time
+from snake import Snake
 
 sc = Screen()
-sc.setup(600,600)
+sc.setup(600, 600)
 sc.bgcolor("black")
 sc.title("My Snake Game :)")
 
-t1 = Turtle("square")
-t1.color("white")
-t2 = Turtle("square")
-t2.setposition((-20, 0))
-t3 = Turtle("square")
+sc.tracer(0) #to stop animation
 
+snake = Snake()
 
+sc.listen()
+sc.onkey(snake.up, "Up")
+sc.onkey(snake.down, "Down")
+sc.onkey(snake.left, "Left")
+sc.onkey(snake.right, "Right")
 
+game_is_on = True
+while game_is_on:
+    sc.update()  # refresh the screen
+    time.sleep(0.1) # piece by piece, ik pice move krega next 0.1 sec bad agla usto 0.1 sec bad
+    snake.move()
 
-
-
+    # game_is_on = False
 
 
 sc.exitonclick()
